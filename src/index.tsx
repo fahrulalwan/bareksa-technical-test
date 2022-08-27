c3bf9ae412e85acbe3dd0edf4f19598cfa113735
Mohammad Fahrul Alwan <fahrulalwan@gmail.com>
2022-08-27 16:42:31 +0700

    add styling assets

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -10,9 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
 
